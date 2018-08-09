579f132a2a2c263ab4364f73e6b812fde0a70dff
ggaribum <joker1649@naver.com>
2018-08-09 17:01:03 +0900

    이분탐색part2수정

---
+++ Algorithm_BinarySearch/src/binarySearch2/Ladder.java
@@ -0,0 +1,12 @@
+package binarySearch2;
+
+public class Ladder {
+
+	//https://www.acmicpc.net/problem/2022
+	
+	public static void main(String[] args) {
+	
+		
+	}
+
+}
---
+++ Algorithm_BinarySearch/src/binarySearch2/WeightLimit.java
@@ -0,0 +1,12 @@
+package binarySearch2;
+
+public class WeightLimit {
+
+	//https://www.acmicpc.net/problem/1939
+	
+	public static void main(String[] args) {
+	
+		
+	}
+
+}
